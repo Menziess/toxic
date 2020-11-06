05c5f7e0552b75c2265245ce80e82a2d48832abf
menziess <stefan_schenk@hotmail.com>
2020-11-06 22:37:45 +0000

    commit 2020_11_06_10_37_PM

--- _nuxt/61be50d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{271:function(n,t,e){"use strict";e.r(t);var l=e(47),component=Object(l.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("hi")])}),[],!1,null,null,null);t.default=component.exports}}]);
