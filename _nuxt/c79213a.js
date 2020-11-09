9ee4b3f7c18c518eed97c94e761814c6abacbbfe
menziess <stefan_schenk@hotmail.com>
2020-11-09 20:13:59 +0000

    commit 2020_11_09_08_13_PM

--- _nuxt/c79213a.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{271:function(e,n,t){"use strict";t.r(n);var l=t(47),component=Object(l.a)({},(function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("We're live!!")])}),[],!1,null,null,null);n.default=component.exports}}]);
